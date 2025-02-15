f4bd19aa50b242671f170b4f91ed518e225d7e43
Mike Maughmer <656278+maughmer@users.noreply.github.com>
2025-02-15 14:45:51 -0800

    added a modal for errors, after watching the instructor's implementation of the project. added some custom css for the modal.

--- src/components/AddProject.jsx
+++ src/components/AddProject.jsx
@@ -1,49 +1,59 @@
 import { useRef, useState } from 'react';
 
 import classes from './AddProject.module.css';
+import Modal from './Modal';
 
 export default function AddProject({ onAdd, onSave }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const date = useRef();
-  const [warning, setWarning] = useState();
+  // const [warning, setWarning] = useState();
 
   function handleSaveProject() {
-    if (title.current.value && description.current.value && date.current.value) {
-      onSave({title: title.current.value, description: description.current.value, date: date.current.value});
+    const enteredTitle = title.current.value.trim();
+    const enteredDesc = description.current.value.trim();
+    const enteredDate = date.current.value;
+
+    if (enteredTitle && enteredDesc && enteredDate) {
+      onSave({title: enteredTitle, description: enteredDesc, date: enteredDate});
     } else {
-      setWarning('Please enter a title, description, and due date.');
+      // setWarning('Please enter a Title, Description, and Due Date.');
+      modal.current.open();
     }
   }
 
-  if (warning && title.current.value && description.current.value && date.current.value) {
-    setWarning('');
-  }
-
   return (
-    <article className={classes.article}>
-      <section>
-        <h2>New Project</h2>
-      </section>
-      <section>
-        <label className="title">Title</label>
-        <input type="text" ref={title} />
-      </section>
-      <section>
-        <label className="title">Description</label>
-        <textarea rows="3" ref={description}></textarea>
-      </section>
-      <section className={classes.columns}>
-        <div>
-          <label className="title">Due Date</label>
-          <input type="date" ref={date} />
-        </div>
-        <div>
-          <button type="button" className="pale" onClick={() => onAdd(false)}>Cancel</button>
-          <button type="button" onClick={handleSaveProject}>Save</button>
-        </div>
-      </section>
-      {warning && <section><label className={classes.warning}>{warning}</label></section>}
-    </article>
+    <>
+      <Modal ref={modal}>
+        <h2>Invalid Input</h2>
+        <p className={classes.warning}>Please enter a Title, Description, and Due Date.</p>
+      </Modal>
+      <article className={classes.article}>
+        <section>
+          <h2>New Project</h2>
+        </section>
+        <section>
+          <label className="title">Title</label>
+          <input type="text" ref={title} />
+        </section>
+        <section>
+          <label className="title">Description</label>
+          <textarea rows="3" ref={description}></textarea>
+        </section>
+        <section className={classes.columns}>
+          <div>
+            <label className="title">Due Date</label>
+            <input type="date" ref={date} />
+          </div>
+          <div>
+            <button type="button" className="pale" onClick={() => onAdd(false)}>Cancel</button>
+            <button type="button" onClick={handleSaveProject}>Save</button>
+          </div>
+        </section>
+        {/* {warning && <section><label className={classes.warning}>{warning}</label></section>} */}
+      </article>
+    </>
   )
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
+
+export default function Modal({ ref, children, buttonName="OK" }) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method="dialog">
+        <button>{buttonName}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root') // from index.html
+  );
+}
